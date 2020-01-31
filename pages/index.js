2ae21493649307be6fb10fa8472804e012cc59bc
Sala <elvisdenis056@gmail.com>
2020-01-31 08:25:59 +0300

    ISS location automatically refreshed after 1 minute

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,10 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const mapStyles = {
-	width: "100%",
-	height: "100%"
+	width: "90vw",
+	height: "84vh",
+	marginLeft: "5vw",
+	border: "2px solid black"
 };
 
 export class MapContainer extends Component {
@@ -19,9 +21,15 @@ export class MapContainer extends Component {
 		};
 	}
 
-	async componentDidMount() {
+	componentDidMount() {
 		this._isMounted = true;
-
+		//fetching data from the API after every 60 seconds
+		this.timerID = setInterval(() => this.fetchingData(), 60000);
+	}
+	componentWillUnmount() {
+		this._isMounted = false;
+	}
+	async fetchingData() {
 		try {
 			const res = await fetch("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/1/&apiKey=4DF924-E6LFYR-EXUUHF-4AEC");
 			const json = await res.json();
@@ -37,34 +45,36 @@ export class MapContainer extends Component {
 			console.log(error);
 		}
 	}
-	componentWillUnmount() {
-		this._isMounted = false;
-	}
 
 	render() {
 		return (
-			<div className="container">
+			<div className="">
 				<Head>
 					<title>ISS Locator</title>
 				</Head>
-				<Header altitude={this.state.altitude} latitude={this.state.latitude} longitude={this.state.longitude} />
+				<div>
+					<Header altitude={this.state.altitude} latitude={this.state.latitude} longitude={this.state.longitude} />
+				</div>
+				<div>
+					<Map
+						google={this.props.google}
+						zoom={6}
+						style={mapStyles}
+						initialCenter={{
+							lat: 1.22392,
+							lng: 36.8665
+						}}
+						onReady={(mapProps, map) => {
+							this.setState({ map: map });
+						}}
+					>
+						<Marker position={{ lat: this.state.latitude, lng: this.state.longitude }} />
+					</Map>
+				</div>
 
-				<Map
-					google={this.props.google}
-					zoom={6}
-					style={mapStyles}
-					initialCenter={{
-						lat: 1.22392,
-						lng: 36.8665
-					}}
-					onReady={(mapProps, map) => {
-						this.setState({ map: map });
-					}}
-				>
-					<Marker position={{ lat: this.state.latitude, lng: this.state.longitude }} />
-				</Map>
-
-				<Footer />
+				{/* <div>
+					<Footer />
+				</div> */}
 			</div>
 		);
 	}
